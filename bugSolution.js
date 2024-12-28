45dd6429ef145c38db8fcf9d6a7d6cfb5951da88
BugHunter-X <askzenai@gmail.com>
2024-12-28 05:54:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.on('error', err => {
+  console.error('Server error:', err);
+  if (err.code === 'EADDRINUSE') {
+    console.error('Port is already in use.');
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
